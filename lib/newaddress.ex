809fde97cf3f646469bb1bbc1d051e1bd168f47d
Meng Guang <mengguang@newtonproject.org>
2020-10-21 15:38:18 +0800

    use snake_case

--- lib/newaddress.ex
+++ lib/newaddress.ex
@@ -3,55 +3,55 @@ defmodule NewChain.Address do
   Documentation for `NewChain.Address`.
   """
 
-  def hexAddress2NewAddress(hexAddress, chainId) when is_binary(hexAddress) and is_integer(chainId) do
-    if(String.length(hexAddress) != 42 || String.starts_with?(hexAddress, "0x") == false) do
-      hexAddress
+  def hex_address_to_new_address(hex_address, chain_id) when is_binary(hex_address) and is_integer(chain_id) do
+    if(String.length(hex_address) != 42 || String.starts_with?(hex_address, "0x") == false) do
+      hex_address
     else
-      {_prefix, address} = String.split_at(hexAddress, 2)
+      {_prefix, address} = String.split_at(hex_address, 2)
       case Base.decode16(address, case: :mixed) do
-        {:ok, binaryAddress} ->
-          hexChainId = Integer.to_string(chainId, 16)
-          binaryChainId = case rem(String.length(hexChainId), 2) do
-            0 -> Base.decode16!(hexChainId, case: :mixed)
-            1 -> Base.decode16!("0" <> hexChainId, case: :mixed)
+        {:ok, binary_address} ->
+          hex_chain_id = Integer.to_string(chain_id, 16)
+          binary_chain_id = case rem(String.length(hex_chain_id), 2) do
+            0 -> Base.decode16!(hex_chain_id, case: :mixed)
+            1 -> Base.decode16!("0" <> hex_chain_id, case: :mixed)
           end
-          "NEW" <> B58.encode58_check!(binaryChainId <> binaryAddress, 0)
+          "NEW" <> B58.encode58_check!(binary_chain_id <> binary_address, 0)
         :error ->
-          hexAddress
+          hex_address
       end
     end
   end
 
-  def newAddress2HexAddress(newAddress) when is_binary(newAddress) do
-    if(String.length(newAddress) < 38 || String.starts_with?(newAddress, "NEW") == false) do
-      newAddress
+  def new_address_to_hex_address(new_address) when is_binary(new_address) do
+    if(String.length(new_address) < 38 || String.starts_with?(new_address, "NEW") == false) do
+      new_address
     else
-      {_prefix, data} = String.split_at(newAddress, 3)
+      {_prefix, data} = String.split_at(new_address, 3)
       case B58.decode58_check(data) do
-        {:ok, {chainIdAndBinaryAddress, _version}} ->
-          <<_chainId :: bytes - size(2), binaryAddress :: bits>> = chainIdAndBinaryAddress
-          "0x" <> Base.encode16(binaryAddress, case: :lower)
+        {:ok, {chain_id_and_binary_address, _version}} ->
+          <<_chainId :: bytes - size(2), binary_address :: bits>> = chain_id_and_binary_address
+          "0x" <> Base.encode16(binary_address, case: :lower)
         {:error, _message} ->
-          newAddress
+          new_address
       end
     end
 
   end
 
-  def trimmedNewAddressEasy(address) do
-    chainId = Application.fetch_env!(:newaddress, :chain_id)
-    newAddress =
+  def trimmed_new_address_easy(address) do
+    chain_id = Application.fetch_env!(:newaddress, :chain_id)
+    new_address =
       address
       |> to_string
-      |> hexAddress2NewAddress(chainId)
-    "#{String.slice(newAddress, 0..7)}–#{String.slice(newAddress, -6..-1)}"
+      |> hex_address_to_new_address(chain_id)
+    "#{String.slice(new_address, 0..6)}-#{String.slice(new_address, -5..-1)}"
   end
 
-  def fullNewAddressEasy(address) do
+  def full_new_address_easy(address) do
     chainId = Application.fetch_env!(:newaddress, :chain_id)
     address
     |> to_string
-    |> hexAddress2NewAddress(chainId)
+    |> hex_address_to_new_address(chainId)
   end
 
 end
